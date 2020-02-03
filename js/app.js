59c3157db1d50b68e94d0a0f998e1c65b13c0d4e
MAlhussaini <MAlhussaini56@gmail.com>
2020-02-03 19:06:00 +0300

    Cat Clicker V2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
 
-let clickCounter = 0;
-let displayedCount = document.getElementById("numOfClicks");
-let clicker = document.getElementById("kitten");
+function counterFunction(displayedCountID, clickerID) {
+    let clickCounter = 0;
+    let displayedCount = document.getElementById(displayedCountID);
+    let clicker = document.getElementById(clickerID);
 
-clicker.addEventListener("click", function() {
-    clickCounter++;
-    if (clickCounter < 10) {
-        clickCounter = "0" + clickCounter;
-    }
-    displayedCount.innerHTML = clickCounter;
-})
+    clicker.addEventListener("click", function() {
+        clickCounter++;
+        if (clickCounter < 10) {
+            clickCounter = "0" + clickCounter;
+        }
+        displayedCount.innerHTML = clickCounter;
+        
+    
+    })    
+}
+
+counterFunction("kittenClicks1", "kitten1");
+counterFunction("kittenClicks2", "kitten2");
