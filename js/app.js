92a73fcbff051ca67d9ab8e0d5b1e8e5f0a8fb07
MAlhussaini <MAlhussaini56@gmail.com>
2020-02-04 20:50:47 +0300

    To the basics

--- js/app.js
+++ js/app.js
@@ -1,19 +1,12 @@
 
-function counterFunction(displayedCountID, clickerID) {
-    let clickCounter = 0;
-    let displayedCount = document.getElementById(displayedCountID);
-    let clicker = document.getElementById(clickerID);
+let clickCounter = 0;
+let displayedCount = document.getElementById("numOfClicks");
+let clicker = document.getElementById("kitten");
 
-    clicker.addEventListener("click", function() {
-        clickCounter++;
-        if (clickCounter < 10) {
-            clickCounter = "0" + clickCounter;
-        }
-        displayedCount.innerHTML = clickCounter;
-        
-    
-    })    
-}
-
-counterFunction("kittenClicks1", "kitten1");
-counterFunction("kittenClicks2", "kitten2");
+clicker.addEventListener("click", function() {
+    clickCounter++;
+    if (clickCounter < 10) {
+        clickCounter = "0" + clickCounter;
+    }
+    displayedCount.innerHTML = clickCounter;
+})
