8ad6c4baf9197d314af7129412f7965f2de426d9
MAlhussaini <MAlhussaini56@gmail.com>
2020-02-07 23:52:31 +0300

    New build to create 5 kittens div element

--- js/app.js
+++ js/app.js
@@ -1,12 +1,32 @@
-
+let img = "img/Carton/kitten.png";
+let tag = "kitten";
 let clickCounter = 0;
-let displayedCount = document.getElementById("numOfClicks");
-let clicker = document.getElementById("kitten");
 
-clicker.addEventListener("click", function() {
-    clickCounter++;
-    if (clickCounter < 10) {
-        clickCounter = "0" + clickCounter;
-    }
-    displayedCount.innerHTML = clickCounter;
-})
+for (let i = 1; i <= 5; i++) {
+    newDivElement = document.createElement("div");
+
+    newTagElement = document.createElement("h1");
+    newTagElement.textContent = tag;
+    newDivElement.appendChild(newTagElement);
+
+    newImgElement = document.createElement("img");
+    newImgElement.setAttribute("src", img);
+    newImgElement.setAttribute("class", "kitten");
+    newImgElement.setAttribute("alt", "cat img");
+    newDivElement.appendChild(newImgElement);
+
+    newCounterElement = document.createElement("h1");
+    newCounterElement.setAttribute("id", "numOfClicks");
+    newCounterElement.textContent = "00";
+    newDivElement.appendChild(newCounterElement);
+
+    newImgElement.addEventListener("click", function() {
+        clickCounter++;
+        if (clickCounter < 10) {
+            clickCounter = "0" + clickCounter;
+        }
+        newCounterElement.innerHTML = clickCounter;
+    })
+
+    document.body.appendChild(newDivElement);
+}
